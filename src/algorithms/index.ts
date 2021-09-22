4ef3e202e50671396caccdaa404fab35d8f1f9fc
slavaMatyuk <vyacheslav.matyuk@innowise-group.com>
2021-09-22 23:34:16 +0300

    feat: algorithms continue

--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -2,20 +2,27 @@ const flattenAndSort = (arr: Array<number>): Array<number> => {
   return arr.flat().sort((a, b) => a - b);
 };
 
-const add = (n) => {
-
+const add = (n: number) => {
+  return function(num: number) {
+    return n + num;
+  }
 };
 
-const multiplyAll = (arr) => {
-
+const multiplyAll = (arr: Array<number>) => {
+  return function(num: number) {
+    return arr.map(el => el * num);
+  }
 };
 
-const uniqueSum = (arr) => {
-
+const uniqueSum = (arr: Array<number>): number => {
+  return [...new Set(arr)].reduce((acc, cur) => acc + cur);
 };
 
-const reduce = (arr, callback, initialValue) => {
-
+const reduce = (arr: [number], callback: (arg0: number, arg1: number) => number, initialValue: number): number => {
+  arr.forEach(el => {
+    initialValue = initialValue !== undefined ? callback(initialValue, el) : el;
+  });
+  return initialValue;
 };
 
 const compose = () => {
