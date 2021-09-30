25737628a3095795c9c6c59dc7a0ea62460cac9f
slavaMatyuk <vyacheslav.matyuk@innowise-group.com>
2021-09-30 14:14:58 +0300

    fix: mentor's remarks - uniqueSum optimize

--- src/algorithms/uniqueSum.ts
+++ src/algorithms/uniqueSum.ts
@@ -1,8 +1,5 @@
 const uniqueSum = (arr: Array<number> | []): number | 0 => {
-  if (arr!.length === 0) {
-    return 0;
-  }
-  return [...new Set(arr)].reduce((acc, cur) => acc + cur);
+  return arr!.length > 0 ? [...new Set(arr)].reduce((acc, cur) => acc + cur) : 0;
 };
 
 export default uniqueSum;
